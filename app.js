0503a0ecee0adf4929ff490a9288c8cd832b8d77
billyATGE <46874350+billyATGE@users.noreply.github.com>
2019-03-14 19:36:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $("button").on("click", function(){
          const searchDiv = $("#here").addClass("card-body");
          
        // searchDiv.append(response);
-         seearchDiv.html(response);
+         searchDiv.html(response);
          //$("#logo").hide();
          //$('img', this).attr('alt', 'google').hide();
          console.log(response);
