bf3443d308ed338a843215fbadcb58143d81259e
Rodolfo Martinez <46171468+RodolfoMartinezMedina@users.noreply.github.com>
2019-03-14 20:51:58 -0400

    Add files via upload

---
+++ coordinates.js
@@ -0,0 +1,28 @@
+var x = document.getElementById("demo");
+var y = "";
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+
+
+  function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "," + position.coords.longitude;
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    GetMap(lat, lng);
+  };
+
+  console.log(GetMap)
+
+  function GetMap(lat, lng) {
+    var map = new Microsoft.Maps.Map('#myMap', {
+      center: new Microsoft.Maps.Location(lat, lng),
+      mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+      zoom: 30,
+    });
+  }
+}
