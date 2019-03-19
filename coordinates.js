84c09bab2fbcfe45efb49530bff9eac7d07fa4bf
billyATGE <46874350+billyATGE@users.noreply.github.com>
2019-03-18 22:37:59 -0400

    Update coordinates.js

--- coordinates.js
+++ coordinates.js
@@ -21,7 +21,8 @@ function getLocation() {
 
   function GetMap(lat, lng) {
     var map = new Microsoft.Maps.Map('#myMap', {
-      center: new Microsoft.Maps.Location(lat, lng),
+      // center: new Microsoft.Maps.Location(lat, lng),
+      center: new Microsoft.Maps.Location(51.50632, -0.12714),
       mapTypeId: Microsoft.Maps.MapTypeId.aerial,
       zoom: 30
     });
